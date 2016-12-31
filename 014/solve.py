dbf7e4a363764ff9ad40e6e58e66db7ed42c6514
birkoff.anarchist <birkoff.anarchist@gmail.com>
2016-12-31 14:10:44 +0700

    problem 14

---
+++ 014/solve.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+longestN = 0
+longest  = 0
+limit = 1000001
+hist = [0 for x in xrange(limit)]
+
+for x in xrange(2, limit):
+    n = x
+    i = 0
+    while n != 1:
+        if n < limit and hist[n] > 0:
+            i += hist[n]
+            break
+        if n%2 == 0:
+            n /= 2
+        else:
+            n = 3*n+1
+        i += 1
+    hist[x] = i
+    if i > longest:
+        longest = i
+        longestN = x
+
+print longestN
